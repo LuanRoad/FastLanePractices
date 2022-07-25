e73813104263fa4a10ba9585367dc6d38c2f7174
Jorge Rugerio <jorge.rugerio@ine.mx>
2022-07-25 11:19:18 -0500

    Primer Coomit + ProyectoBase
    Primer Coomit + ProyectoBase

---
+++ HelloiOS/HelloiOS/HelloiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOSApp.swift
+//  HelloiOS
+//
+//  Created by Administrador on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
