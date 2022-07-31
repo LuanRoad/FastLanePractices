47e18c966fa81b43422116216c4b433c8a0ec7be
Jorge Rugerio <jorge.rugerio@ine.mx>
2022-07-31 12:14:05 -0500

    fastlane app_center - Adhoc
    fastlane app_center - Adhoc

--- HelloiOS/HelloiOS/ContentView.swift
+++ HelloiOS/HelloiOS/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world - fastlane match")
+        Text("Hello, world - fastlane appcenter! - AdHoc")
             .padding()
     }
 }
