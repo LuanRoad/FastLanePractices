286da9086d235225ab490f91fa62663b0f195778
Jorge Rugerio <jorge.rugerio@ine.mx>
2022-08-04 16:47:12 -0500

    JenkinsConfig
    JenkinsConfig

--- HelloiOS/HelloiOS/ContentView.swift
+++ HelloiOS/HelloiOS/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world - fastlane Firebase! - AdHoc")
+        Text("Hello, world - fastlane Firebase Service Account! - AdHoc")
             .padding()
     }
 }
