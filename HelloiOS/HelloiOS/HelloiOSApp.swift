3c2131144d71dc83b046a5df98650402d978ff50
Jorge Rugerio <jorge.rugerio@ine.mx>
2022-08-11 13:51:24 -0500

    Regresando el proyecto a su estado original
    Regresando el proyecto a su estado original

---
+++ HelloiOS/HelloiOS/HelloiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOSApp.swift
+//  HelloiOS
+//
+//  Created by Administrador on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
